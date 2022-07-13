41ecccfa9873decd92d086a05d4df6ee1a6fc10f
Jeremy Lane <lane203j@gmail.com>
2022-07-13 11:15:12 -0400

    added code for plotting forecast. minor changes to docstrings, typing, and code improvements.

--- dashboard/app/main.py
+++ dashboard/app/main.py
@@ -4,20 +4,24 @@ from dash import Dash, html, dcc
 from flask import Flask
 from visualize import postseason_race
 
-description = '''
-  Created by [Jeremy Lane](https://github.com/lanej5). [View source on github](https://github.com/lanej5/mlb). Data is updated daily around 10AM ET.   
-'''
-
 server = Flask(__name__)
 
+# get data
+fig, created = postseason_race()
+code_src = "[View source on github](https://github.com/lanej5/mlb)."
+data_src = "Data from [FanGraphs](https://www.fangraphs.com/)."
+updated = f"Last updated {created.strftime('%B %-d, %Y at %-I:%M %p')}"   
+text = " ".join([code_src, data_src, updated])
+
+# create the dashboard
 app = Dash(__name__, server=server)
 app.title = 'Postseason Race'
 app.layout = html.Div(children=[
   dcc.Graph(
     id='postseason_race',
-    figure=postseason_race()
+    figure=fig
   ),
-  dcc.Markdown(children=description)
+  dcc.Markdown(children=text)
 ])
 
 if __name__ == '__main__':
--- dashboard/app/visualize.py
+++ dashboard/app/visualize.py
@@ -2,59 +2,91 @@
 """Generate plotly figure visualizing the postseason race."""
 from datetime import datetime, timedelta
 from collections import OrderedDict
-from typing import List, Dict
+from typing import OrderedDict as OrderedDictType
+from typing import List, Dict, Tuple
 
 import plotly.graph_objects as go
 
-import load
 
+ScatterDict = OrderedDictType[str, go.Scatter]
 
-def generate_traces(teams: Dict) -> OrderedDict[go.Scatter]:
+def generate_traces(teams: Dict[str, Dict]) -> ScatterDict:
   """Generate traces for the plot.
 
   Args:
-    teams: Team data.
+  -----
+    teams: Dict of data for each team.
+      
+      Keys: team abbr (e.g., 'TOR'). 
+      Values: Dicts containing keys:
+        'name': full team name, e.g., 'New York Yankees'
+        'league': e.g., 'American League (AL)'
+        'div': e.g., 'AL East'
+        'color': html code for the team colour, e.g., '#C4CED3'
+        'record': List[int] containing the wins over 500 for each day
+          in the season so far. This is what is plotted. Length is number
+          of days elapsed in the season to date.
+        'wins': int, total wins.
+        'losses': int, total losses.
+        'rank': int, rank within division according to FanGraphs.
+          Ties are possible.
+        'forecast' (optional): List[int] containing wins over 500 for a
+          given number of days beyond the current day.
 
   Returns:
+  --------
     OrderedDict of plotly Scatter objects.
+      
+      Keys: team abbr. 
+      Values: go.Scatter object.
+      
+      Dict is ordered according to 'rank' so that teams are listed by
+      rank in the legend.
   """
   traces = OrderedDict()
 
-  dates = [datetime(2022, 4, 7) + timedelta(days=i) for i in range(len(teams['TOR']['record']))]
-
   # add line plots of team records
-  # teams are added in descending order so they will 
-  # appear according to their rank in the legend
+  # teams are sorted by rank so that legend is ordered
   for team, data in sorted(teams.items(), key=lambda x: x[1]['rank']):
-    trace_name = data['name'] + ' (' + str(data['wins']) + '-' + str(data['losses']) + ')'
+    trace_name = f"{data['name']} ({str(data['wins'])}-{str(data['losses'])})"
     
+    # x-axis and y=-axis values for the plot
+    y_vals = data['record']
+    if 'forecast' in data:
+       y_vals += data['forecast']
+    x_vals = [
+      datetime(2022, 4, 7) + timedelta(days=i) for i in range(len(y_vals))
+    ]
+
     traces[team] = go.Scatter(
-      x=dates,
-      y=data['record'],
+      x=x_vals,
+      y=y_vals,
       mode='lines',
-      visible=(data['div'] == 'AL East'),
+      visible=(data['div'] == 'AL East'), # only AL East visible on page load
       legendgroup=data['div'],
       legendgrouptitle_text=data['div'],
       name=trace_name,
       line_color=data['color']
     )
+
   return traces
 
-def generate_buttons(traces: OrderedDict[go.Scatter], teams: Dict) -> List:
+def generate_buttons(traces: ScatterDict, teams: Dict[str, Dict]) -> List[Dict]:
   """Generate buttons for dropdown menu.
 
   Args:
-    traces: Traces to be plotted.
-    teams: Team data.
+  -----
+    traces: Traces to be plotted. See generate_traces().
+    teams: Team data. See generate_traces().
 
   Returns:
-    List of buttons, in the order they appear in the menu.
+  --------
+    List of button dicts in the order they appear in the menu.
   """
 
   buttons = []
 
   # american league buttons
-
   # al division buttons
   al_divisions = ['AL East', 'AL Central', 'AL West']
   buttons += [
@@ -94,7 +126,6 @@ def generate_buttons(traces: OrderedDict[go.Scatter], teams: Dict) -> List:
   )
 
   # national league buttons
-
   # nl division buttons
   nl_divisions = ['NL East', 'NL Central', 'NL West']
   buttons += [
@@ -135,13 +166,25 @@ def generate_buttons(traces: OrderedDict[go.Scatter], teams: Dict) -> List:
 
   return buttons
 
-def postseason_race() -> go.Figure:
+def postseason_race(dashboard_data: Dict=None) -> Tuple[go.Figure, datetime]:
   """Generate plotly figure visualizing the 2022 postseason race.
 
+  Args:
+  -----
+    dashboard_data (optional): pass dashboard data directly instead of loading
+      from bucket. For testing purposes.
+
   Returns:
-    A plotly figure.
+  --------
+    plotly figure.
+    datetime that the data was collected.
   """
-  dashboard_data = load.dashboard_data(test=False)
+  if dashboard_data is None:
+    import load
+    dashboard_data = load.dashboard_data()
+
+  date_created = datetime.strptime(dashboard_data['created'], "%Y-%m-%d")
+
   traces = generate_traces(dashboard_data['teams'])
   buttons = generate_buttons(traces, dashboard_data['teams'])
 
@@ -169,11 +212,30 @@ def postseason_race() -> go.Figure:
 
   fig = go.Figure(data=list(traces.values()), layout=layout)
 
-  # fix the axis ranges so that selecting buttons doesn't shift things around
+  # fix axis ranges so that selecting buttons doesn't shift things around
   fig.update_xaxes(range=[datetime(2022, 4, 7), datetime(2022, 10, 2)])
 
-  y_min = min([min(data['record']) for data in dashboard_data['teams'].values()])
-  y_max = max([max(data['record']) for data in dashboard_data['teams'].values()])
-  fig.update_yaxes(range=[y_min - 2, y_max + 2])
+  y_min = min([min(trace['y']) for trace in traces.values()]) - 2
+  y_max = max([max(trace['y']) for trace in traces.values()]) + 2
 
-  return fig
+  fig.update_yaxes(range=[y_min, y_max])
+
+  if 'forecast' in dashboard_data['teams']['TOR']:
+    forcast_length = len(dashboard_data['teams']['TOR']['forecast'])
+
+    record_end = date_created - timedelta(days=1)
+    fig.add_vline(
+        x=record_end,
+        line_width=1.5,
+        line_dash="dash",
+        line_color="grey"
+    )
+    fig.add_vrect(
+        x0=record_end,
+        x1=record_end + timedelta(days=forcast_length),
+        line_width=0,
+        fillcolor="grey",
+        opacity=0.2
+    )
+
+  return fig, date_created
